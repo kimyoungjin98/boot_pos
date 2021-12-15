cd04f2fb3c5f340032f6cb8bca0febf33ccf270b
kimyoungjin98 <gyu250@naver.com>
2021-12-15 19:55:23 +0900

    start

---
+++ src/main/kotlin/com/youngjin/pos/ServletInitializer.kt
@@ -0,0 +1,12 @@
+package com.youngjin.pos
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+class ServletInitializer : SpringBootServletInitializer() {
+
+	override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+		return application.sources(PosApplication::class.java)
+	}
+
+}
---
+++ src/test/kotlin/com/youngjin/pos/PosApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.youngjin.pos
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PosApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
