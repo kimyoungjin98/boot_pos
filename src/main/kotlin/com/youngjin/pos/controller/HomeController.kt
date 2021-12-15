67f08424694a3f537058e2fd1d40ccae6b7377c3
kimyoungjin98 <gyu250@naver.com>
2021-12-15 20:15:13 +0900

    설정:

---
+++ src/main/kotlin/com/youngjin/pos/controller/HomeController.kt
@@ -0,0 +1,16 @@
+package com.youngjin.pos.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+
+@Controller
+class HomeController {
+
+    @RequestMapping(value=["/"], method = [RequestMethod.GET])
+    fun home():String  {
+
+        return "home"
+    }
+
+}
